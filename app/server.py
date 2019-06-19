ed3a4e5db487af3cc69d56682b2c7f50fa581662
pkrouth <pkrouth@users.noreply.github.com>
2019-06-19 03:58:52 -0400

    Working Configurations

--- app/server.py
+++ app/server.py
@@ -1,35 +1,26 @@
+
+import uvicorn, aiohttp, asyncio
+
 from starlette.applications import Starlette
-from starlette.responses import HTMLResponse, JSONResponse
+from starlette.responses import PlainTextResponse, HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
-import uvicorn, aiohttp, asyncio
-from io import BytesIO
 
 from fastai import *
 from fastai.text import *
 
-model_file_url = 'https://drive.google.com/open?id=10MN520gXqIDnqJPSq5jURdgXtzDqU56e'
-model_file_name = 'export'
-path = Path(__file__).parent
 
 app = Starlette()
+app.debug = True
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
-app.mount('/static', StaticFiles(directory='app/static'))
-
-async def download_file(url, dest):
-    if dest.exists(): return
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.read()
-            with open(dest, 'wb') as f: f.write(data)
+app.mount('/static', StaticFiles(directory="app/static"))
+model_file_url = 'https://www.dropbox.com/s/3y8ht35rxr0s1ao/model_exported.pkl?dl=0'
+model_file_name = 'model_exported'
+path = Path(__file__).parent
 
 async def setup_learner():
     await download_file(model_file_url, path/'models'/f'{model_file_name}.pkl')
-    #learn = cnn_learner(data_bunch, models.resnet34, pretrained=False)
-    #learn = text_classifier_learner(data_bunch, AWD_LSTM, pretrained=False)
-    #learn = get_t
-    learn =load_learner(path/'models',f'{model_file_name}.pkl')
-    #learn.load(model)
+    learn =load_learner(path/'models','model_exported.pkl')
     return learn
 
 loop = asyncio.get_event_loop()
@@ -37,17 +28,26 @@ tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
+
+
 @app.route('/')
 def index(request):
     html = path/'view'/'index.html'
     return HTMLResponse(html.open().read())
 
+
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
     data = await request.form()
-    img_bytes = await (data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    return JSONResponse({'result': learn.predict(img)[0]})
+    class_, ind, probs = learn.predict(data['unique_name'])
+    score = probs[ind].item()
+    if score < 0.5:
+        Confidence='Low'
+    else:
+        Confidence='High'
+    return JSONResponse({'label': str(class_),'score': float(score), 'Confidence':Confidence}) # This remains same
+
+
 
 if __name__ == '__main__':
-    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
+    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8008)
