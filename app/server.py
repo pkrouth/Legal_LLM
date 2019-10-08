3c1529f28c0c2e20c2bd92ad70826e50c61195bb
Prahlad <prahladkrouth@gmail.com>
2019-10-08 12:17:24 -0400

    GCP Running Stage

--- app/server.py
+++ app/server.py
@@ -50,4 +50,4 @@ async def analyze(request):
 
 
 if __name__ == '__main__':
-    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8008)
+    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
